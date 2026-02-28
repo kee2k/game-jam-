1a87aca8594e793196f31f6d03a1f1004eebe368
kee2k <2189142@sd61learn.ca>
2026-02-28 11:09:14 -0800

    adding player movement

---
+++ game-jam-/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const ACCEL = 2.0
+
+var input: Vector2
+
+func get_imput():
+	input.x = Input.get_action_strength("right") - Input.get_action_strength ("left")
+	input.y = Input.get_action_strength("down") - Input.get_action_strength ("up")
+	return input.normalized()
+
+func _process(delta):
+	var playerInput = get_imput()
