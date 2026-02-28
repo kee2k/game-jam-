1507046bd47e617a3a75a630d917a8a06deeb747
kee2k <2189142@sd61learn.ca>
2026-02-28 14:18:14 -0800

    added character

---
+++ gamejam/character_body_2d.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+@export var SPEED := 300.0
+@export var acceleration := 10.0
+
+var animation_direction: String = "down"
+var animation_state: String = ""
+
+func update_sprite_direction(input: Vector2) -> void:
+	match input:
+		Vector2.DOWN:
+			animation_direction = "down"
+		Vector2.UP:
+			animation_direction = "up"
+		Vector2.RIGHT:
+			animation_direction = "right"
+		Vector2.LEFT:
+			animation_direction = "left"
+
+	
+
+func update_sprite() -> void:
+	if velocity.length() > 0:
+		animation_state = "move_"
+	else:
+		animation_state = "idle_"
+
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("left", "right", "up", "down")
+	
+	update_sprite_direction(direction)
+	update_sprite()
+	
+	animated_sprite_2d.play(animation_state+animation_direction)
+	
+	velocity.x = move_toward(velocity.x, direction.x*SPEED, acceleration)
+	velocity.y = move_toward(velocity.y, direction.y*SPEED, acceleration)
+	
+	move_and_collide(velocity*delta)
